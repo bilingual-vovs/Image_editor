9a863a6bae9be94bb019c71f6848a03185334277
bilingual-vovs <vovavovsart@gmail.com>
2025-02-10 14:50:33 +0200

    Draw

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import html2canvas from 'html2canvas'
 import React, { Component } from 'react'
 import CanvasImage from './Components/CanvasImage'
 import RandomDivs from './Components/RandomDivs'
+import Workspace from './Components/Workspace'
+import Header from './Components/Header'
 
 export default class App extends Component {
   constructor(){
     super()
     this.state = {
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJxo2NFiYcR35GzCk5T3nxA7rGlSsXvIfJwg&s"
+      tool: "none"
     }
   }
   renderContent = () => {
     const element = document.getElementById('render')
     html2canvas(element, {useCORS: true}).then(
-      (dat)=>{
+        (dat)=>{
         let data = dat.toDataURL('image.jpg')
         let link = document.createElement('a');
         link.href = data;
@@ -22,26 +24,19 @@ export default class App extends Component {
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
-      }
+        }
     )
+}
+  onDraw = () => {
+    this.setState({tool: (this.state.tool == "draw" ? "none":"draw")})
   }
+
   render() {
     return (
       <div id='main'>
-        <div id="render">
-            <h1>Megatest</h1>
-            <img src={this.state.src} alt="" />
-            <div style={{backgroundColor: "black"}}>
-            <h1 style={{color: "White"}}>Mabambam</h1>
-            <input onChange={this.inp} type="text" name="" id="inp" />
-            {/* <RandomDivs/> */}
-            </div>
-        </div>
-        <button onClick={this.renderContent}>Download</button>
+        <Header onDraw = {this.onDraw} onDownload = {this.renderContent}/>
+        <Workspace tool={this.state.tool}/>
       </div>
     )
-  }
-  inp = (e) => {
-    this.setState({src: e.target.value})
   }
 }
