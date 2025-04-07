66932d45e1a76a55ae93290ef365a0860a64dbdc
Vovs <74075592+bilingual-vovs@users.noreply.github.com>
2025-04-07 21:57:09 +0300

    AA

--- src/Components/CanvasLayer/CanvasLayer.jsx
+++ src/Components/CanvasLayer/CanvasLayer.jsx
@@ -1,46 +1,100 @@
 import React, { Component } from 'react'
+import "./style.css"
+import { SketchPicker } from 'react-color';
 
-export default class CanvasLayer extends Component {
-    canvasRef = React.createRef();
+class CanvasLayer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brushColor: '#000000',
+      brushWidth: 5,
+      drawing: false,
+      shape: 'freehand', // Options: freehand, rectangle, circle, triangle
+    };
+    this.canvasRef = React.createRef();
+  }
 
-    size = 1
-  
-    paint = (e) => {
-        const canvas = this.canvasRef.current;
-        if (!canvas) {
-            return; 
-        }
-        if (!this.m) return
-        const context = canvas.getContext("2d");
-        context.lineWidth = this.size
-        const { left, top } = canvas.getBoundingClientRect();
-        const x = e.clientX - left;
-        const y = e.clientY - top;
-        context.lineTo(x, y);   
-        context.stroke();
-    }; 
-    up = () => {
-        this.m = false
-    }
-    down = (evt) => {
-        const canvas = this.canvasRef.current;
-        if (!canvas) {
-            return; 
-        }
-        
-        const context = canvas.getContext("2d");
-        context.moveTo(evt.clientX, evt.clientY)
-        this.m = true
-    }
-    chngVal = (evt) => {
-        this.size = evt.target.value
-    }
+  handleMouseDown = (e) => {
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    this.setState({ drawing: true });
+  };
+
+  handleMouseMove = (e) => {
+    if (!this.state.drawing) return;
+    const canvas = this.canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.strokeStyle = this.state.brushColor;
+    ctx.lineWidth = this.state.brushWidth;
+    ctx.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    ctx.stroke();
+  };
+
+  handleMouseUp = () => {
+    this.setState({ drawing: false });
+  };
+
+  handleColorChange = (color) => {
+    this.setState({ brushColor: color.hex });
+  };
+
+  handleBrushWidthChange = (e) => {
+    this.setState({ brushWidth: e.target.value });
+  };
+
+  handleShapeChange = (shape) => {
+    this.setState({ shape });
+  };
+
   render() {
     return (
-      <div className='canvas-layer'>
-        <canvas width={1000} height={1000} ref={this.canvasRef} onMouseMove={this.paint} onMouseUp={this.up} onMouseDown={this.down}></canvas>
-        <input onChange={this.chngVal}  id='size' type="text" />
+      <div className="canvas-container">
+        <div className="sidebar">
+          <h3>Controls</h3>
+          <div>
+            <label>Brush Width:</label>
+            <input
+              type="number"
+              value={this.state.brushWidth}
+              onChange={this.handleBrushWidthChange}
+              min="1"
+              max="50"
+            />
+          </div>
+          <div>
+            <label>Brush Color:</label>
+            <SketchPicker
+              color={this.state.brushColor}
+              onChangeComplete={this.handleColorChange}
+            />
+          </div>
+          <div>
+            <label>Shape:</label>
+            <select
+              value={this.state.shape}
+              onChange={(e) => this.handleShapeChange(e.target.value)}
+            >
+              <option value="freehand">Freehand</option>
+              <option value="rectangle">Rectangle</option>
+              <option value="circle">Circle</option>
+              <option value="triangle">Triangle</option>
+            </select>
+          </div>
+        </div>
+        <canvas
+          ref={this.canvasRef}
+          className="drawing-canvas"
+          width={800}
+          height={600}
+          onMouseDown={this.handleMouseDown}
+          onMouseMove={this.handleMouseMove}
+          onMouseUp={this.handleMouseUp}
+        ></canvas>
       </div>
-    )
+    );
   }
 }
+
+export default CanvasLayer;
